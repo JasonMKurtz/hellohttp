ae15a889bf20d67e5fb77953d0264e685352f945
Jason Kurtz <jason@jkurtz.net>
2020-06-13 14:51:41 -0400

    add 'automated' message

--- src/hello.go
+++ src/hello.go
@@ -22,6 +22,6 @@ func GetIP(r *http.Request) string {
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
     host, _ := os.Hostname()
-    fmt.Fprintf(w, "Hello, this is %s, and you are %s", host, GetIP(r))
+    fmt.Fprintf(w, "Hello, this is %s, and you are %s\n", host, GetIP(r))
     fmt.Fprintf(w, "This is the first autobuild test.")
 }
