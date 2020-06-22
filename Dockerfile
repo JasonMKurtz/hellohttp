6698da40d504daadca9446e052b9a5d7b1e8c854
Jason Kurtz <jason@jkurtz.net>
2020-06-22 10:58:46 -0400

    Upgraded app.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM golang:1 as builder
 WORKDIR /tmp/hello
-COPY src/hello.go .
-RUN go build -o hello hello.go
+COPY src/routes/ .
+RUN go build -o hello *.go
 
 FROM ubuntu:18.04
 COPY --from=builder /tmp/hello/hello /hello
 RUN chmod +x ./hello
-EXPOSE 8080
+EXPOSE 8000
 CMD ["/hello"]
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -12,10 +12,15 @@ type Route struct {
 
 type Routes struct {
 	routes []Route
+	port   string
+}
+
+func (r *Routes) addRoute(route Route) {
+	r.routes = append(r.routes, route)
 }
 
 func (r *Routes) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	for _, route := range getRoutes().routes {
+	for _, route := range r.routes {
 		if req.URL.Path == route.route {
 			route.handler(w, req)
 			return
@@ -26,18 +31,16 @@ func (r *Routes) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	return
 }
 
-func getRoutes() *Routes {
-	routes := []Route{
-		Route{route: "/hello", handler: HandleHello},
-		Route{route: "/bar", handler: HandleBar},
-	}
-
-	return &Routes{routes: routes}
-}
-
 func main() {
-	fmt.Printf("Listening...")
-	http.ListenAndServe(":9000", getRoutes())
+	r := &Routes{port: "8000"}
+	r.addRoute(Route{route: "/hello", handler: HandleHello})
+	r.addRoute(Route{route: "/bar", handler: HandleBar})
+
+	fmt.Printf("Listening on :%s...\n", r.port)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", r.port), r)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
 }
 
 func HandleHello(w http.ResponseWriter, r *http.Request) {
