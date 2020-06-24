def15dbc8a00ded4a7d31ed8023262b07018d328
Jason Kurtz <jason@jkurtz.net>
2020-06-24 11:30:25 -0400

    now with regex routes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 )
 
-type RouteHandler func(w http.ResponseWriter, r *http.Request)
+type RouteHandler func(w http.ResponseWriter, r *http.Request, route string)
 
 type Route struct {
 	route   string
@@ -15,6 +16,7 @@ type Route struct {
 type Routes struct {
 	routes  []Route
 	port    string
+	primary RouteHandler
 	missing RouteHandler
 }
 
@@ -23,10 +25,7 @@ func (r *Routes) addRoute(route Route) {
 }
 
 func (r *Routes) addDefaultRoute(h RouteHandler) {
-	r.addRoute(Route{
-		route:   "/",
-		handler: h,
-	})
+	r.primary = h
 }
 
 func (r *Routes) add404Route(h RouteHandler) {
@@ -35,14 +34,19 @@ func (r *Routes) add404Route(h RouteHandler) {
 
 func (r *Routes) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	for _, route := range r.routes {
-		if req.URL.Path == route.route {
-			route.handler(w, req)
+		reg, _ := regexp.Compile(route.route)
+		path := req.URL.Path
+		if path == "/" && r.primary != nil {
+			r.primary(w, req, "/")
+			return
+		} else if reg.MatchString(path) {
+			route.handler(w, req, route.route)
 			return
 		}
 	}
 
 	if r.missing != nil {
-		r.missing(w, req)
+		r.missing(w, req, "")
 		return
 	}
 
@@ -51,11 +55,10 @@ func (r *Routes) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	r := &Routes{port: "8080"}
-	r.addDefaultRoute(HandleHello)
-	r.add404Route(Missing)
+	r := &Routes{port: "8080", primary: HandleHello, missing: Missing}
 	r.addRoute(Route{route: "/hello", handler: HandleHello})
 	r.addRoute(Route{route: "/bar", handler: HandleBar})
+	r.addRoute(Route{route: "^/greet/(?P<name>.+)$", handler: Greet})
 
 	fmt.Printf("Listening on :%s...\n", r.port)
 	err := http.ListenAndServe(fmt.Sprintf(":%s", r.port), r)
@@ -64,14 +67,19 @@ func main() {
 	}
 }
 
-func Missing(w http.ResponseWriter, r *http.Request) {
+func Greet(w http.ResponseWriter, r *http.Request, route string) {
+	reg := &JRegex{route, r.URL.Path}
+	fmt.Fprintf(w, "Hello %s!\n", reg.GetNamedGroups()["name"])
+}
+
+func Missing(w http.ResponseWriter, r *http.Request, route string) {
 	fmt.Fprintf(w, r.URL.Path+" was requested but not found.")
 }
 
-func HandleHello(w http.ResponseWriter, r *http.Request) {
+func HandleHello(w http.ResponseWriter, r *http.Request, route string) {
 	fmt.Fprintf(w, "Hello, this might be /hello!")
 }
 
-func HandleBar(w http.ResponseWriter, r *http.Request) {
+func HandleBar(w http.ResponseWriter, r *http.Request, route string) {
 	fmt.Fprintf(w, "Hello, this is /bar!")
 }
