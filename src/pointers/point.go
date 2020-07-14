bf72a663d6577ab4c01ba8be7d303c82fe28ead1
Jason Kurtz <jason@jkurtz.net>
2020-07-14 13:34:50 -0400

    More fun with linked lists; now deletable elements.

--- src/pointers/point.go
+++ src/pointers/point.go
@@ -15,6 +15,23 @@ type Post struct {
 	prev  *Post
 }
 
+type Seeker int
+
+const (
+	top Seeker = iota
+	prev
+	next
+	bottom
+)
+
+func (s Seeker) IsValid() bool {
+	switch s {
+	case top, prev, next, bottom:
+		return true
+	}
+	return false
+}
+
 func CreateFeed() *Feed {
 	return &Feed{}
 }
@@ -43,14 +60,28 @@ func (f *Feed) AddPost(title, text string) {
 	f.curPost = &p
 }
 
-func (f *Feed) DelPost(title string) {
+func (f *Feed) Delete(title string) {
 	for _, p := range f.List() {
-		if p.title == title {
+		if p.title != title {
+			continue
+		}
+
+		next := p.next
+		prev := p.prev
+		f.length--
+		if next == nil && prev == nil { // deleting our only element
+			f.topPost = nil
+			f.curPost = nil
+		} else if next == nil { // bottom of the list
+			prev.next = nil
+		} else if prev == nil { // top of the list
+			next.prev = nil
+			f.topPost = next
+		} else { // some middle element
 			p.prev.next = p.next
-			f.curPost = p
+			p.next.prev = p.prev
 		}
 	}
-	f.length--
 }
 
 func (f *Feed) List() []*Post {
@@ -69,39 +100,49 @@ func (f *Feed) List() []*Post {
 	return posts
 }
 
-func (f *Feed) Tail() *Post {
-	last := f.curPost
-	for last.next != nil {
-		last = last.next
+func (f *Feed) seek(direction Seeker) {
+	if !direction.IsValid() {
+		return
 	}
 
-	return last
-}
-
-func (f *Feed) seek(direction int) {
-	if direction == 0 {
+	switch direction {
+	case top:
+		f.curPost = f.topPost
+		break
+	case prev:
 		prev := f.curPost.prev
 		if prev == nil {
 			return
 		}
 		f.curPost = prev
-	} else if direction == 1 {
+		break
+	case next:
 		next := f.curPost.next
 		if next == nil {
 			return
 		}
 		f.curPost = next
+		break
+	case bottom:
+		f.curPost = f.Tail()
+		break
 	}
 }
 
-func (f *Feed) Prev() *Post {
-	f.seek(0)
-	return f.curPost
+func (f *Feed) SeekPrev() {
+	f.seek(prev)
 }
 
-func (f *Feed) Next() *Post {
-	f.seek(1)
-	return f.curPost
+func (f *Feed) SeekNext() {
+	f.seek(next)
+}
+
+func (f *Feed) SeekHead() {
+	f.seek(top)
+}
+
+func (f *Feed) SeekTail() {
+	f.seek(bottom)
 }
 
 func (f *Feed) Top() *Post {
@@ -110,6 +151,15 @@ func (f *Feed) Top() *Post {
 
 func (f *Feed) Current() *Post {
 	return f.curPost
+}
+
+func (f *Feed) Tail() *Post {
+	last := f.curPost
+	for last.next != nil {
+		last = last.next
+	}
+
+	return last
 }
 
 func ReadPost(p *Post) string {
@@ -137,12 +187,7 @@ func main() {
 	f := CreateFeed()
 	f.AddPost("test post", "lorem")
 	f.AddPost("test post 2", "lorem")
-	fmt.Printf(ReadPost(f.Current()))
-	f.Prev()
-	fmt.Printf(ReadPost(f.Current()))
-	f.AddPost("test post 3", "lorem")
-	f.AddPost("test post 4", "lorem")
 	for _, p := range f.List() {
-		fmt.Printf(ReadPost(p))
+		fmt.Printf("%s\n", ReadPost(p))
 	}
 }
